ab59794573080143d377d26b53b8ab7bcca88521
Greta Skipare <gretchfurlined@gmail.com>
2021-05-27 12:54:04 +0300

    init

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 // Make a request for a user with a given ID
 axios.get('https://jsonplaceholder.typicode.com/users')
-  .then(function (response) {
-    let names = response.data.map(user => user.name);
-    let ulElement = document.getElementsByTagName("ul")[0];
-    //let result = objArray.map(a => a.foo);
-    // handle success
-    let liHtml = "";
-    for (let i = 0; i < names.length; i++) {
-        const name = names[i];
-        liHtml += `<li>${name}</li>`;
-    }
+    .then(function(response) {
+        console.log(response.data);
+        let table = document.getElementById('table');
+        let HTML = `        
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>username</th>
+          <th>email</th>
+          <th>phone</th>
+          <th>website</th>
+          <th>company</th>
+          <th>address</th>
+          <th>test</th>
+        </tr>`;
 
-    ulElement.innerHTML = liHtml;
+        // console.log(Object.values(response.data[0]));
+        response.data.forEach(user => {
+            HTML +=
+                `<tr>
+                  <td> ${user.id} </td>
+                  <td> ${user.name} </td>
+                  <td> ${user.username} </td>
+                  <td> ${user.email} </td>
+                  <td> ${user.phone} </td>
+                  <td> ${user.website} </td>
+                  <td>
+                    <ul>
+                      <li>${user.company.name}</li>
+                      <li>${user.company.catchPhrase}</li>
+                      <li>${user.company.bs}</li> 
+                    </ul>
+                  </td>
+                  <td>
+                    <ol>
+                      <li>${user.address.city}</li>
+                      <li>${user.address.street}</li>
+                      <li>${user.address.suite}</li> 
+                      <li>${user.address.zipcode}</li> 
+                      <li>
+                        <ul>
+                          <li>${user.address.geo.lat}</li> 
+                          <li>${user.address.geo.lng}</li> 
+                        </ul>
+                      </li> 
+                    </ol>
+                  </td>
+                </tr>`;
+        });
+        table.innerHTML = HTML;
 
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+    })
+    .catch(function(error) {
+        // handle error
+        console.log(error);
+    });
 
-  //1. make a variable where we store the names of received array of objects
-  //2. Select the <ul> html element from document.
-  //3. for loop to go trough every name to append the name to ul document.
-
-
+//1. make a variable where we store the names of received array of objects
+//2. Select the <ul> html element from document.
+//3. for loop to go trough every name to append the name to ul document.
